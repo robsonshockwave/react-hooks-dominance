6364f312d0a3e9ce89bc06acfaa68545629b3dfd
Robson <robsonshockwavegithub@gmail.com>
2021-07-01 21:00:02 -0300

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
+import { useReducer } from 'react';
 import './App.css';
-import { AppContext } from './contexts/AppContext';
-import { Div } from './components/Div';
+
+const globalState = {
+  title: 'O título que contexto',
+  body: 'O body do contexto',
+  counter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'muda': {
+      console.log('Chamou muda com', action.payload);
+      return { ...state, title: action.payload };
+    }
+    case 'inverter': {
+      console.log('Chamou inverter');
+      const { title } = state;
+      return { ...state, title: title.split('').reverse().join('') };
+    }
+  }
+
+  console.log('NENHUM ACTION ENCONTRADA');
+  return { ...state }; //sempre tem que retornar um estado
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, globalState);
+  const { counter, title, body } = state;
+
   return (
-    <AppContext>
-      <Div />
-    </AppContext>
+    <div>
+      <h1>
+        {title} {body}
+      </h1>
+      <button onClick={() => dispatch({ type: 'muda', payload: new Date().toLocaleString('pt-br') })}>Click</button>
+      <button onClick={() => dispatch({ type: 'inverter' })}>Invert</button>
+      <button onClick={() => dispatch({ type: 'QUALQUERCOISA' })}>SEM ACTION</button>
+    </div>
   );
 }
 
